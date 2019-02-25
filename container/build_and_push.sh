d26371ed31d1a05349cb897a0a2d9254ff4594e9
Pratik Sheth <25923780+daftengineer@users.noreply.github.com>
2019-02-25 12:21:33 +0530

    Update build_and_push.sh

--- container/build_and_push.sh
+++ container/build_and_push.sh
@@ -13,8 +13,8 @@ then
     exit 1
 fi
 
-chmod +x decision_trees/train
-chmod +x decision_trees/serve
+chmod +x kerasmodel/train
+chmod +x kerasmodel/serve
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
